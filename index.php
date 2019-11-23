26a6be39ec6f47e0415e59399717c1279a4886de
CharithKularatna <charithkularatna@gmail.com>
2019-11-24 00:41:15 +0530

    Host https added with a config file test 2

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
     require('config.php');
 
-    echo $DB_HOST;
-    $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
+    echo "Hi";
+    /*$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
@@ -10,4 +10,5 @@
     $sql = "select name,email from user";
     $result = $conn->query($sql);
     echo $sql;
+    */
 ?>
