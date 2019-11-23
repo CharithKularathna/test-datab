4ff012cea8295626338880bca356456c0bd0f19c
CharithKularatna <charithkularatna@gmail.com>
2019-11-24 00:27:32 +0530

    DBName not included correction

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     require('config.php');
 
 
-    $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD);
+    $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
