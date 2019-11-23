34277b7fbb5b31e2d1f2bd1d7ff7944bd2c56595
CharithKularatna <charithkularatna@gmail.com>
2019-11-24 00:44:26 +0530

    Constants with NO $

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
     require('config.php');
-
-    echo DB_NAME;
-    /*$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
@@ -10,5 +8,5 @@
     $sql = "select name,email from user";
     $result = $conn->query($sql);
     echo $sql;
-    */
+    
 ?>
