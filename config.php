73e47fe5a238c0a3a70ece7fb5a2bd000158d59a
CharithKularatna <charithkularatna@gmail.com>
2019-11-24 00:33:14 +0530

    Host https added

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 //DB Constants/Params
-define("DB_HOST","www.us-cdbr-iron-east-05.cleardb.net");
+define("DB_HOST","https://us-cdbr-iron-east-05.cleardb.net");
 define("DB_USER","bdde6f907f7b57");
 define("DB_PASSWORD","70a5e9ec");
 define("DB_NAME","heroku_de2f18f953c712d");
